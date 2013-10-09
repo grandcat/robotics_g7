97ccd66c8aee1adb462165f12ac3525698fe8747
Olivier <balland.oliv@gmail.com>
2013-10-09 15:10:40 +0200

    Hand tracking with Primesense

--- camera/src/controller.cpp
+++ camera/src/controller.cpp
@@ -16,8 +16,8 @@ ros::Publisher cmd_pub;
 
 
 const int depth0 = 500;
-const int alpha = 10;
-const int beta = 10;
+const float alpha = 0.1;
+const float beta = 0.1;
 void receive_cam(const Position::ConstPtr &msg)
 {
 	int pos = msg->x;
@@ -29,8 +29,8 @@ void receive_cam(const Position::ConstPtr &msg)
 	}
 
 	Speed speed_msg;
-	speed_msg.W1 = alpha*pos + beta*depth;
-	speed_msg.W2 = -alpha*pos + beta*depth;
+	speed_msg.W1 = alpha*pos - beta*depth;
+	speed_msg.W2 = -alpha*pos - beta*depth;
 	speed_msg.header.stamp = ros::Time::now();
 	cmd_pub.publish(speed_msg);
 }
@@ -40,7 +40,7 @@ int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "Controller");
 	ros::NodeHandle nh;
-	cmd_pub = nh.advertise<Speed>("/motors/pwm",100);
+	cmd_pub = nh.advertise<Speed>("/motion/Speed",100);
 	ros::Subscriber pos_sub = nh.subscribe("/pos",100,receive_cam);
 
 	ros::Rate loop_rate(100);
