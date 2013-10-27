9851bd8b948ec10ec2cf40b6b2b72eb1d458ec85
Olivier <balland.oliv@gmail.com>
2013-10-27 22:47:47 +0100

    Milestone 1

--- robot/src/EKF.cpp
+++ robot/src/EKF.cpp
@@ -180,8 +180,8 @@ int main(int argc, char** argv)
 
 
 	// Init
-	sigma = 1E-6 * MatrixXd::Identity(4,4);
-	R = 1E-6 * MatrixXd::Identity(4,4);
+	sigma = 1E-8 * MatrixXd::Identity(4,4);
+	R = 1E-8 * MatrixXd::Identity(4,4);
 	R(3,3) = 1E-2;
 	Q = 1E-4;
 	G = MatrixXd::Identity(4,4);
