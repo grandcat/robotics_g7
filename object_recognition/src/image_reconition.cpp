e4675803dfc6f5b21d7d4712d20958da89f9f518
robo <robo@robo-VirtualBox.(none)>
2013-11-09 16:39:00 +0100

    feature detection

--- object_recognition/src/image_reconition.cpp
+++ object_recognition/src/image_reconition.cpp
@@ -5,61 +5,46 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
 #include <stdlib.h>
 #include <stdio.h>
 
 using namespace cv;
 namespace enc = sensor_msgs::image_encodings;
 
-//static const char WINDOW[] = "Image window";
-idImg testImg[];
 
-
-
-struct idImg 
-{
-	Mat img;		
-	int obj;
-}
-
-struct imgHist 
+struct ImgHist 
 {
 	Mat bHist;
 	Mat gHist;
 	Mat rHist;
-}
+};
 
-
-
-/*
- * Try to identify the object by comparing edge detection to train images
- */
-void identifyObject(Mat edges)
+struct Features
 {
+	vector<KeyPoint> keypoints;
+	Mat descriptors;
+};
 
-}
-
-
-/*
- * Train on some images
- */
-void trainOnImages()
+struct IdImg 
 {
-
-	testImg[0] = { .img = imread("/testimages/1_1.jpg"), .obj = 1 };
-	testImg[1] = { .img = imread("/testimages/2_1.jpg"), .obj = 2 };
-	testImg[2] = { .img = imread("/testimages/3_1.jpg"), .obj = 3 };
-	testImg[3] = { .img = imread("/testimages/3_2.jpg"), .obj = 3 };
-	testImg[4] = { .img = imread("/testimages/3_3.jpg"), .obj = 3 };
-	testImg[5] = { .img = imread("/testimages/4_1.jpg"), .obj = 4 };
-	testImg[6] = { .img = imread("/testimages/4_2.jpg"), .obj = 4 };
-	testImg[7] = { .img = imread("/testimages/4_3.jpg"), .obj = 4 };
-
-}
+	Mat img;
+	int obj;
+	ImgHist colorHist;
+	//Mat edges;
+	Features feat;
+};
 
 
 
-void colorDetection(Mat matImg)
+//static const char WINDOW[] = "Image window";
+const int trainSize = 4;
+IdImg testImg[trainSize];
+
+
+ImgHist colorDetection(Mat matImg)
 {
 	//Split image into planes (B, G, R)
 	vector<Mat> planes;
@@ -80,39 +65,15 @@ void colorDetection(Mat matImg)
 	calcHist(&planes[1], 1, 0, Mat(), gHist, 1, &histSize, &histRange, uniform, accumulate);
 	calcHist(&planes[2], 1, 0, Mat(), rHist, 1, &histSize, &histRange, uniform, accumulate);
 
-
-	// Draw histograms (for testing)
-	int histW = 512; 
-	int histH = 400;
-	int binW = cvRound( (double) histW/histSize );
-
-	Mat histImage( histH, histW, CV_8UC3, Scalar( 0,0,0) );
-
-	normalize(bHist, bHist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-	normalize(gHist, gHist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-	normalize(rHist, rHist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-
-	for( int i = 1; i < histSize; i++ )
-	{
-	  line( histImage, Point( binW*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
-		               Point( binW*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
-		               Scalar( 255, 0, 0), 2, 8, 0  );
-	  line( histImage, Point( binW*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
-		               Point( binW*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
-		               Scalar( 0, 255, 0), 2, 8, 0  );
-	  line( histImage, Point( binW*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
-		               Point( binW*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
-		               Scalar( 0, 0, 255), 2, 8, 0  );
-	}
-	namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE );
-	imshow("calcHist Demo", histImage );
+	ImgHist hist = {bHist, gHist, rHist};
+	return hist;
 }
 
 
 /*
  * Perform Canny Edge Detection on the image
  */
-void edgeDetection(Mat img)
+Mat edgeDetection(Mat img)
 {
 	//noise reduction with gaussian filter
 	Mat edges;
@@ -130,11 +91,97 @@ void edgeDetection(Mat img)
 	finalImg = Scalar::all(0);
 	img.copyTo(finalImg, edges);
 
-	imshow("Edge map", finalImg);
+	//imshow("Edge map", finalImg);
 
-	identifyObject(edges);
+	return finalImg;
 }
 
+
+/*
+ * Detect features in image by finding keypoints
+ */
+Features featureDetector(Mat img)
+{
+  Ptr<FeatureDetector> featureDetector = FeatureDetector::create("SURF");
+  vector<KeyPoint> keypoints;
+  featureDetector->detect(img, keypoints);
+
+	Mat descriptors;
+  Ptr<DescriptorExtractor> featureExtractor = DescriptorExtractor::create("SURF");
+  featureExtractor->compute(img, keypoints, descriptors);
+
+	/*Mat outputImage;
+  Scalar keypointColor = Scalar(255, 0, 0);
+  drawKeypoints(img, keypoints, outputImage, keypointColor, DrawMatchesFlags::DEFAULT);
+
+  namedWindow("Output");
+  imshow("Output", outputImage);*/
+
+	Features feat = { keypoints, descriptors };
+	return feat;
+}
+
+
+/*
+ * Try to identify the object by comparing edge detection to train images
+ */
+void identifyObject(ImgHist hist, Features feat)
+{
+	int bestMatch = 0;
+	double diff = 0.3;
+	for(int i=0; i < trainSize; ++i) {
+		double resB = compareHist(hist.bHist, testImg[i].colorHist.bHist, 3);
+		double resG = compareHist(hist.gHist, testImg[i].colorHist.gHist, 3);
+		double resR = compareHist(hist.rHist, testImg[i].colorHist.rHist, 3);
+
+		if(resB < diff && resG < diff && resR < diff) {
+			bestMatch = testImg[i].obj;
+			std::cout << "Best match: " << bestMatch << std::endl;
+			std::cout << "Histogram compare B,G,R: " << resG << ", " << resB << ", " << resR << std::endl;
+		}
+	}
+
+	if(bestMatch == 0) {
+		std::cout << "No match" << std::endl;
+	}
+
+	// matching descriptors
+	BFMatcher matcher(cv::NORM_L2, true);
+	vector<cv::DMatch> matches;
+	matcher.match(feat.descriptors, testImg[0].feat.descriptors, matches);
+
+	std::cout << "Matches: " << matches.size() << std::endl;
+}
+
+
+/*
+ * Train on some images
+ */
+void train()
+{
+	ImgHist dummyHist;
+	Features edgeDummy;
+	testImg[0] = { imread("src/testimages/1_1.jpg"), 1, dummyHist, edgeDummy };
+	testImg[1] = { imread("src/testimages/2_1.jpg"), 2, dummyHist, edgeDummy };
+	testImg[2] = { imread("src/testimages/3_1.jpg"), 3, dummyHist, edgeDummy };
+	//testImg[3] = { imread("src/testimages/3_2.jpg"), 3, dummyHist, edgeDummy };
+	//testImg[4] = { imread("src/testimages/3_3.jpg"), 3, dummyHist, edgeDummy };
+	testImg[3] = { imread("src/testimages/4_1.jpg"), 4, dummyHist, edgeDummy };
+	//testImg[6] = { imread("src/testimages/4_2.jpg"), 4, dummyHist, edgeDummy };
+	//testImg[7] = { imread("src/testimages/4_3.jpg"), 4, dummyHist, edgeDummy };
+
+	Mat img = imread( "src/testimage.jpg" );
+	for(int i=0; i < trainSize; ++i) {
+		resize(testImg[i].img, testImg[i].img, img.size());
+
+		testImg[i].colorHist = colorDetection(testImg[i].img);
+
+		testImg[i].feat = featureDetector(testImg[i].img);
+
+		/*cvtColor( testImg[i].img, testImg[i].img, CV_BGR2GRAY );
+		testImg[i].edges = edgeDetection(testImg[i].img);*/
+	}
+}
 
 
 int main( int argc, char** argv )
@@ -142,17 +189,45 @@ int main( int argc, char** argv )
 	ros::init(argc, argv, "recognition");
 	ros::NodeHandle n;
 
+	initModule_nonfree();
+
+	// Train on test images
+	train();
+
 	// Load an image
+	Mat img = imread( "src/testimage.jpg" );
 	Mat src = imread( argv[1] );
+	resize(src, src, img.size());
 
 	if( !src.data )
 	{ return -1; }
 
+	// Color detection
+	ImgHist hist = colorDetection(src);
+
+	//Feature detection
+	Features feat = featureDetector(src);
+
 	/// Convert the image to grayscale
 	cvtColor( src, src, CV_BGR2GRAY );
 
-	/// Show the image
-	edgeDetection(src);
+	// Canny Edge Detector
+	Mat edges = edgeDetection(src);
+
+	// Identify object
+	identifyObject(hist, feat);
+
+
+	// matching descriptors
+	/*BFMatcher matcher(cv::NORM_L2, true);
+	vector<cv::DMatch> matches;
+	matcher.match(feat.descriptors, testImg[0].feat.descriptors, matches);
+
+	// drawing the results
+	namedWindow("matches", 1);
+	Mat img_matches;
+	drawMatches(src, feat.keypoints, testImg[0].img, testImg[0].feat.keypoints, matches, img_matches);
+	imshow("matches", img_matches);*/
 
 	/// Wait until user exit program by pressing a key
 	waitKey(0);
--- object_recognition/src/recognition.cpp
+++ object_recognition/src/recognition.cpp
@@ -13,59 +13,6 @@ namespace enc = sensor_msgs::image_encodings;
 static const char WINDOW[] = "Image window";
 
 
-void colorDetection(IplImage img)
-{
-    Mat matImg(img);
-
-	//Split image into planes (B, G, R)
-	vector<Mat> planes;
-	split( matImg, planes );
-
-	int histSize = 256;
-
-	// Set ranges for B,G,R
-	float range[] = { 0, 256 } ; //the upper boundary is exclusive
-	const float* histRange = { range };
-	
-	bool uniform = true; 
-	bool accumulate = false;
-
-	// Calculate histograms for B,G,R
-	Mat bHist, gHist, rHist;
-	calcHist(&planes[0], 1, 0, Mat(), bHist, 1, &histSize, &histRange, uniform, accumulate);
-	calcHist(&planes[1], 1, 0, Mat(), gHist, 1, &histSize, &histRange, uniform, accumulate);
-	calcHist(&planes[2], 1, 0, Mat(), rHist, 1, &histSize, &histRange, uniform, accumulate);
-
-
-	// Draw histograms (for testing)
-	int histW = 512; 
-	int histH = 400;
-	int binW = cvRound( (double) histW/histSize );
-
-	Mat histImage( histH, histW, CV_8UC3, Scalar( 0,0,0) );
-
-	normalize(bHist, bHist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-	normalize(gHist, gHist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-	normalize(rHist, rHist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-
-	for( int i = 1; i < histSize; i++ )
-	{
-	  line( histImage, Point( binW*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
-		               Point( binW*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
-		               Scalar( 255, 0, 0), 2, 8, 0  );
-	  line( histImage, Point( binW*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
-		               Point( binW*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
-		               Scalar( 0, 255, 0), 2, 8, 0  );
-	  line( histImage, Point( binW*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
-		               Point( binW*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
-		               Scalar( 0, 0, 255), 2, 8, 0  );
-	}
-	namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE );
-	imshow("calcHist Demo", histImage );
-}
-
-
-
 
 /*
  * Perform Canny Edge Detection on the image
@@ -145,7 +92,7 @@ public:
   }
 };
 
-
+/*
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "image_converter");
@@ -153,4 +100,4 @@ int main(int argc, char** argv)
   ros::spin();
   return 0;
 }
-
+*/
