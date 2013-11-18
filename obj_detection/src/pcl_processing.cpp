c9d0e694ff8a183f0578e583325f9b1c26f971fd
Stefan Smarzly <smarzlys@gmail.com>
2013-11-18 14:39:02 +0100

    Framework for PCL processing - only cut off points which are too far away

--- obj_detection/src/pcl_processing.cpp
+++ obj_detection/src/pcl_processing.cpp
@@ -8,69 +8,61 @@
 #include "headers/pcl_processing.hpp"
 
 #include <ros/ros.h>
-#include <pcl_ros/point_cloud.h>		//< will be detected when reimported in eclipse
+#include <pcl_ros/point_cloud.h>
 #include <sensor_msgs/PointCloud2.h>
+// Point Clouds
+#include <pcl/ros/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
 
-#include <opencv/cv.h>
-#include <opencv/cxcore.h>
-#include <opencv/highgui.h>
-#include "math.h"
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
 #include <sstream>
 
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-
-namespace enc = sensor_msgs::image_encodings;
-
 using namespace std;
 
-class ImageConverter
+ros::Subscriber sub_pcl_primesense;
+ros::Publisher pub_pcl_filtered;
+
+/**
+ * @brief process_pcl_data
+ * @param plc_raw
+ */
+void process_pcl_data(const sensor_msgs::PointCloud2ConstPtr& plc_raw)
 {
-	ros::NodeHandle nh_;
-	ros::Subscriber pcl_recv;
-	image_transport::Subscriber image_sub_depth_;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr pclCloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::fromROSMsg(*plc_raw, *pclCloud);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
 
-public:
-	IplImage* img;
+  // Reduce visible points to close view
+  pcl::PassThrough<pcl::PointXYZ> pass;
+  pass.setInputCloud(pclCloud);
+  pass.setFilterFieldName("z");
+  pass.setFilterLimits(0.0, 1.0);
+  pass.filter(*cloud_filtered);
 
-	ImageConverter()
-	{
-		ROS_INFO("Starting pcl converter.");
-		pcl_recv = nh_.subscribe<sensor_msgs::PointCloud2>("/camera/depth/points", 1,
-				&ImageConverter::process_pcl_data, this);
-		ROS_INFO("Subscribed to depth pointcloud.");
-	}
-
-	~ImageConverter()
-	{
-//		cvReleaseImage(&img);
-//		cvReleaseImage(&img_depth);
-	}
-
-	void process_pcl_data(const sensor_msgs::PointCloud2ConstPtr& cloud) {
-
-	}
-};
+  sensor_msgs::PointCloud2 output;
+  pcl::toROSMsg(*cloud_filtered, output);
+  pub_pcl_filtered.publish(output);
+}
 
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "PCL Processing");
-	ros::NodeHandle nh;
+  ros::init(argc, argv, "pcl_processing");
+  ros::NodeHandle nh;
 
-	ImageConverter ic;
-	ros::spin();
+  ROS_INFO("Starting pcl converter.");
 
-	cvDestroyAllWindows();
+  // Create a ROS subscriber for thle input point cloud
+  sub_pcl_primesense = nh.subscribe("/camera/depth_registered/points", 3, process_pcl_data);
+  pub_pcl_filtered = nh.advertise<sensor_msgs::PointCloud2>("/camera/filtered_points", 3);
+  ROS_INFO("Subscribed to depth pointcloud.");
 
-	return 0;
+  ros::spin();
+  return 0;
 }
-
 
 
