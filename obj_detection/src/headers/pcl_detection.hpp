741148f55c7b439789d05b78ae6372b16c8bfc6f
Stefan Smarzly <smarzlys@gmail.com>
2013-11-21 02:50:36 +0100

    Prepare for new helper tool: extracting objects from big point clouds

--- obj_detection/src/headers/pcl_detection.hpp
+++ obj_detection/src/headers/pcl_detection.hpp
@@ -13,44 +13,49 @@
 namespace objRecognition
 {
 
-  class ImageFetchSmooth
+class ImageFetchSmooth
+{
+public:
+  ImageFetchSmooth(ros::NodeHandle& nh) : nh_(nh), processingActive(false)
   {
-  public:
-    ImageFetchSmooth(ros::NodeHandle& nh) : nh_(nh), processingActive(false)
-    {
-      // Subscribe to Primesense camera and publish debugging data
-      sub_pcl_primesense = nh_.subscribe("/camera/depth_registered/points", 1,
-                                        &objRecognition::ImageFetchSmooth::rcvPointCloud, this);
-      pub_pcl_filtered = nh_.advertise<sensor_msgs::PointCloud2>("/camera/filtered_points", 1);
-      ROS_INFO("pcl_detection: Subscribed to pointcloud data.");
+    // Subscribe to Primesense camera and publish debugging data
+    sub_pcl_primesense = nh_.subscribe("/camera/depth_registered/points", 1,
+                                       &objRecognition::ImageFetchSmooth::rcvPointCloud, this);
+    pub_pcl_filtered = nh_.advertise<sensor_msgs::PointCloud2>("/camera/filtered_points", 1);
+    ROS_INFO("pcl_detection: Subscribed to pointcloud data.");
 
-    }
+  }
 
-    /**
-     * @brief rcvPointCloud Process point clouds as soon as start() was triggered
-     * @param pc_raw  Point cloud data from camera
-     */
-    void rcvPointCloud(const sensor_msgs::PointCloud2ConstPtr& pc_raw);
+  /**
+   * @brief rcvPointCloud Process point clouds as soon as start() was triggered
+   * @param pc_raw  Point cloud data from camera
+   */
+  void rcvPointCloud(const sensor_msgs::PointCloud2ConstPtr& pc_raw);
 
-    /**
-     * @brief start Start point cloud processing for next frames
-     *  Robot should stay at one place for smoothing & recognition process
-     */
-    void start()
-    {
-      processingActive = true;
-    }
+  /**
+   * @brief start Start point cloud processing for next frames
+   *  Robot should stay at one place for smoothing & recognition process
+   */
+  void start()
+  {
+    processingActive = true;
+  }
 
-  private:
-    // ROS connection
-    ros::NodeHandle& nh_;
-    ros::Subscriber sub_pcl_primesense;
-    ros::Publisher pub_pcl_filtered;
+  bool getProcessStatus() const
+  {
+    return processingActive;
+  }
 
-    bool processingActive;
-    // Point cloud data
-    pcl::PointCloud<pcl::PointXYZ>::Ptr pclFiltered;
-  };
+private:
+  // ROS connection
+  ros::NodeHandle& nh_;
+  ros::Subscriber sub_pcl_primesense;
+  ros::Publisher pub_pcl_filtered;
+
+  bool processingActive;
+  // Point cloud data
+  pcl::PointCloud<pcl::PointXYZ>::Ptr pclFiltered;
+};
 
 }
 
--- obj_detection/src/pcl_detection.cpp
+++ obj_detection/src/pcl_detection.cpp
@@ -17,30 +17,31 @@
 
 namespace objRecognition
 {
-  void ImageFetchSmooth::rcvPointCloud(const sensor_msgs::PointCloud2ConstPtr &pc_raw)
-  {
-    if (!processingActive)
-      {
-        ROS_INFO("Ignore pointcloud frame (not started!)");
 
-        return;
-      }
+void ImageFetchSmooth::rcvPointCloud(const sensor_msgs::PointCloud2ConstPtr &pc_raw)
+{
+  if (!processingActive)
+    {
+      ROS_INFO("Ignore pointcloud frame (not started!)");
 
-    // Reduce view 100cm to the front (Z axes) and downsample amount of points
-    sensor_msgs::PointCloud2 cloudVoxel;
-    pcl::VoxelGrid<sensor_msgs::PointCloud2> pclVoxelFilter;
-    pclVoxelFilter.setInputCloud(pc_raw);
-    pclVoxelFilter.setFilterFieldName("z");
-    pclVoxelFilter.setFilterLimits(0.0, 1.0);  //< only take XYZ points maximum 1m away from camera on z axis
-    pclVoxelFilter.setLeafSize(VOXEL_LEAF_SIZE, VOXEL_LEAF_SIZE, VOXEL_LEAF_SIZE);
-    pclVoxelFilter.filter(cloudVoxel);
+      return;
+    }
 
-    // Convert to PCL data representation for more advanced treatment of Pointclouds
-    pcl::PointCloud<pcl::PointXYZ>::Ptr pclFiltered(new pcl::PointCloud<pcl::PointXYZ>),
-        pclProcessed(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::fromROSMsg(cloudVoxel, *pclFiltered);
+  // Reduce view 100cm to the front (Z axes) and downsample amount of points
+  sensor_msgs::PointCloud2 cloudVoxel;
+  pcl::VoxelGrid<sensor_msgs::PointCloud2> pclVoxelFilter;
+  pclVoxelFilter.setInputCloud(pc_raw);
+  pclVoxelFilter.setFilterFieldName("z");
+  pclVoxelFilter.setFilterLimits(0.0, 1.0);  //< only take XYZ points maximum 1m away from camera on z axis
+  pclVoxelFilter.setLeafSize(VOXEL_LEAF_SIZE, VOXEL_LEAF_SIZE, VOXEL_LEAF_SIZE);
+  pclVoxelFilter.filter(cloudVoxel);
 
-    ROS_INFO("Points filtered: amount->%i", pclFiltered->width * pclFiltered->height);
+  // Convert to PCL data representation for more advanced treatment of Pointclouds
+  pcl::PointCloud<pcl::PointXYZ>::Ptr pclFiltered(new pcl::PointCloud<pcl::PointXYZ>),
+      pclProcessed(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::fromROSMsg(cloudVoxel, *pclFiltered);
+
+  ROS_INFO("Points filtered: amount->%i", pclFiltered->width * pclFiltered->height);
 
   //  pcl::PointCloud<pcl::PointXYZ>::Ptr cloudNoiseFree(new pcl::PointCloud<pcl::PointXYZ>);
   //  pcl::StatisticalOutlierRemoval<pcl::PointXYZ> pclOutlierFilter;
@@ -49,32 +50,32 @@ namespace objRecognition
   //  pclOutlierFilter.setStddevMulThresh(1.0);
   //  pclOutlierFilter.filter(*cloudNoiseFree);
 
-    // Determine plane surfaces (only vertical walls)
-    Eigen::Vector3f axisBackPlane = Eigen::Vector3f(0.0, 0.0, 1.0);
+  // Determine plane surfaces (only vertical walls)
+  Eigen::Vector3f axisBackPlane = Eigen::Vector3f(0.0, 0.0, 1.0);
 
-    pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
-    pcl::PointIndices::Ptr inlierIndices(new pcl::PointIndices);
-    pcl::SACSegmentation<pcl::PointXYZ> pclSegmentation;
-    pclSegmentation.setInputCloud(pclFiltered);
-    pclSegmentation.setOptimizeCoefficients(true);
-    pclSegmentation.setModelType(pcl::SACMODEL_PERPENDICULAR_PLANE);
-    pclSegmentation.setMethodType(pcl::SAC_RANSAC);
-    pclSegmentation.setAxis(axisBackPlane);     // only check for
-    pclSegmentation.setEpsAngle(12.0f * (M_PI/180.0f));
-    pclSegmentation.setDistanceThreshold(0.02); // how close point must be to object to be inlier
-    pclSegmentation.setMaxIterations(1000);
-    pclSegmentation.segment(*inlierIndices, *coefficients); //< TODO: nothing detected, take another frame
-    // Remove points of possible walls
-    pcl::ExtractIndices<pcl::PointXYZ> pclObstacle;
-    pclObstacle.setInputCloud(pclFiltered);
-    pclObstacle.setIndices(inlierIndices);
-    pclObstacle.setNegative(true);    // inverse: remove walls
-    pclObstacle.filter(*pclProcessed);
+  pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+  pcl::PointIndices::Ptr inlierIndices(new pcl::PointIndices);
+  pcl::SACSegmentation<pcl::PointXYZ> pclSegmentation;
+  pclSegmentation.setInputCloud(pclFiltered);
+  pclSegmentation.setOptimizeCoefficients(true);
+  pclSegmentation.setModelType(pcl::SACMODEL_PERPENDICULAR_PLANE);
+  pclSegmentation.setMethodType(pcl::SAC_RANSAC);
+  pclSegmentation.setAxis(axisBackPlane);     // only check for
+  pclSegmentation.setEpsAngle(12.0f * (M_PI/180.0f));
+  pclSegmentation.setDistanceThreshold(0.02); // how close point must be to object to be inlier
+  pclSegmentation.setMaxIterations(1000);
+  pclSegmentation.segment(*inlierIndices, *coefficients); //< TODO: nothing detected, take another frame
+  // Remove points of possible walls
+  pcl::ExtractIndices<pcl::PointXYZ> pclObstacle;
+  pclObstacle.setInputCloud(pclFiltered);
+  pclObstacle.setIndices(inlierIndices);
+  pclObstacle.setNegative(true);    // inverse: remove walls
+  pclObstacle.filter(*pclProcessed);
 
-    // Generate debugging output for rViz
-    sensor_msgs::PointCloud2 output;
-    pcl::toROSMsg(*pclProcessed, output);
-    pub_pcl_filtered.publish(output);
-  }
+  // Generate debugging output for rViz
+  sensor_msgs::PointCloud2 output;
+  pcl::toROSMsg(*pclProcessed, output);
+  pub_pcl_filtered.publish(output);
+}
 
 }
